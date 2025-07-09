7fe6fe48b0cbfa17dbe51dd7b27ac0a84f3c8a9c
Leonche04 <leo.marchand04@gmail.com>
2025-07-09 09:51:45 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,33 +1,22 @@
 export default async function handler(req, res) {
-  const boomiUrl = "https://c01-deu.integrate-test.boomi.com/ws/simple/getLuccaLogin";
-  const auth = Buffer.from(process.env.BOOMI_AUTH).toString('base64');
+  const boomiUrl = "https://c01-deu.integrate-test.boomi.com/ws/simple/getLuccaLogin?user=leo.marchand@dalkiaelectrotechnics.fr";
 
-  if (req.method === "POST") {
-    let body = "";
-    req.on("data", chunk => {
-      body += chunk.toString();
-    });
+  const auth = Buffer.from("boomitest@dalkiaelectrotechnics-MK4UKY.KWC3QR:47c6ab20-838c-49fa-b148-9238b36f85ef").toString('base64');
 
-    req.on("end", async () => {
-      const response = await fetch(boomiUrl, {
-        method: "POST",
-        headers: {
-          Authorization: `Basic ${auth}`,
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: body,
-        redirect: "follow"
-      });
+  const response = await fetch(boomiUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Basic ${auth}`
+    },
+    redirect: 'follow'
+  });
 
-      if (response.redirected) {
-        res.writeHead(302, { Location: response.url });
-        res.end();
-      } else {
-        const text = await response.text();
-        res.status(response.status).send(text);
-      }
-    });
+  // Redirige vers la réponse finale si 3xx, sinon affiche un message
+  if (response.redirected) {
+    res.writeHead(302, { Location: response.url });
+    res.end();
   } else {
-    res.status(405).send("Méthode non autorisée");
+    const text = await response.text();
+    res.status(response.status).send(text);
   }
 }
